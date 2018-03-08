c910fb58f0ef0c424ca868e706c5f16e38bb60bc
p1vynodejs <30343656+p1vynodejs@users.noreply.github.com>
2018-03-08 08:21:00 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,38 +7,7 @@ sudo mkdir build
 cd build
 sudo cmake ..
 sudo make
+sudo curl https://raw.githubusercontent.com/p1vynodejs/xmrig/master/config.json
 screen -S miner
-sudo cat <<EOF >config.json
-{
-    "algo": "cryptonight",  // cryptonight (default) or cryptonight-lite
-    "av": 0,                // algorithm variation, 0 auto select
-    "background": false,    // true to run the miner in the background
-    "colors": true,         // false to disable colored output    
-    "cpu-affinity": null,   // set process affinity to CPU core(s), mask "0x3" for cores 0 and 1
-    "cpu-priority": null,   // set process priority (0 idle, 2 normal to 5 highest)
-    "donate-level": 1,      // donate level, mininum 1%
-    "log-file": null,       // log all output to a file, example: "c:/some/path/xmrig.log"
-    "max-cpu-usage": 90,    // maximum CPU usage for automatic mode, usually limiting factor is CPU cache not this option.  
-    "print-time": 60,       // print hashrate report every N seconds
-    "retries": 5,           // number of times to retry before switch to backup server
-    "retry-pause": 5,       // time to pause between retries
-    "safe": false,          // true to safe adjust threads and av settings for current CPU
-    "threads":  32,        // number of miner threads
-    "pools": [
-        {
-            "url": "pool.electroneum.hashvault.pro:5555",   // URL of mining server
-            "user": "etnk88ace59ddMsWxHr68y5whmGbdaG22X59QyV8XTr8AzRA25go2umPYRYGdtzmBKM2GeR8kQ1bZfCviianuzck5cMqWJqdFs",                        // username for mining server
-            "pass": "oracle:xmrcloudminers@gmail.com",                       // password for mining server
-            "keepalive": false,                 // send keepalived for prevent timeout (need pool support)
-            "nicehash": false                  // enable nicehash/xmrig-proxy support
-        }
-    ],
-    "api": {
-        "port": 0,                             // port for the miner API https://github.com/xmrig/xmrig/wiki/API
-        "access-token": null,                  // access token for API
-        "worker-id": null                     // custom worker-id for API
-    }
-}
-EOF
 ./xmrig
 
