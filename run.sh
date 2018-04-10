a1cd5e43436f7b39236978672105412c76069c7b
p1vynodejs <30343656+p1vynodejs@users.noreply.github.com>
2018-04-10 22:14:23 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update
 sudo sudo apt-get install git build-essential cmake libuv1-dev libmicrohttpd-dev -y
-sudo git clone -b feature-cryptonight-heavy https://github.com/xmrig/xmrig.git
+sudo git clone https://github.com/xmrig/xmrig.git
 cd xmrig
 sudo mkdir build
 cd build
